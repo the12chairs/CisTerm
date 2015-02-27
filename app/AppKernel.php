3a6e5a76281737aded23329f6e3c64b88fbff4fb
Maksim Bondarenko <maksim.bondarenko@noveogroup.com>
2015-02-27 18:55:19 +0600

    UserBundle added

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,9 +16,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new ApiBundle\ApiBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle($this),
+            new FOS\UserBundle\FOSUserBundle(),
+            new CisTerm\ApiBundle\CisTermApiBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
