f8e46ce31c81a0c35285d44c0256986981733939
Maksim Bondarenko <maksim.bondarenko@noveogroup.com>
2015-03-02 12:28:01 +0600

    Missing files added

---
+++ src/CisTerm/ApiBundle/CisTermApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CisTerm\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CisTermApiBundle extends Bundle
+{
+}
---
+++ src/CisTerm/ApiBundle/Controller/ApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CisTerm\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class ApiController extends Controller
+{
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     */
+    public function indexAction($name)
+    {
+        return array('name' => $name);
+    }
+}
